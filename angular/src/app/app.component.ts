a35cfda943977397ce29212b39111eed5a14c0f1
Ahmed El Sayegh <ahmedelsayegh7@gmail.com>
2018-03-07 23:26:29 +0200

    Add reordering buttons

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   title = 'app';
   todos = [];
   input = "";
+
   submit() {
     this.todos.push(this.input);
     this.input = "";
@@ -16,6 +17,14 @@ export class AppComponent {
 
   delete(todo) {
     this.todos.splice(this.todos.indexOf(todo), 1);
+  }
+
+  move(places, index) {
+    if (index + places < 0 || index + places >= this.todos.length) return;
+    let todoInPlace = this.todos[index + places];
+    let todoToBeMoved = this.todos[index];
+    this.todos[index] = todoInPlace;
+    this.todos[index + places] = todoToBeMoved;
   }
 
 }
