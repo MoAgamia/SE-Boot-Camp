205a7007c9e23ca2e32f4b24a558acf587c441ce
Ahmed El Sayegh <ahmedelsayegh7@gmail.com>
2018-04-02 16:41:02 +0200

    Styled body

--- angular/src/app/todos/todos.component.ts
+++ angular/src/app/todos/todos.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private as: AuthService, private router: Router) { }
   header: String = "todolist"
 
   ngOnInit() {
+    if (!this.as.authorized) {
+      this.router.navigate(['/login']);
+    }
   }
 
 }
